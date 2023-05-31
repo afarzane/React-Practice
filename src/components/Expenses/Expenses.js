e9fde7e2c07373feb7d23f559f52461d99fd679a
Amirali Farzaneh <afarzane@sfu.ca>
2023-05-31 00:29:06 -0700

    Added an annual filter for grouping expenses.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from './ExpenseFilter';
 import Card from '../UI/Card';
 import './Expenses.css';
 
 function Expenses(props){
+    const [filtYear, setFiltYear] = useState('2023');
+    
+    function updateFilter (selectedYear) {
+        setFiltYear(selectedYear);
+    }
+    
     return(
         <Card className="expenses">
+        <ExpenseFilter selected={filtYear} onChangeFilter={updateFilter}/>
             <ExpenseItem 
             title={props.expenses[0].title} 
             amount={props.expenses[0].amount} 
